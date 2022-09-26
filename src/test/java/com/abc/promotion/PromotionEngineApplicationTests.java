b9262225b32cf35fdd3e2723f812e6841647b67c
shreeharshasl <shree.h1990@gmail.com>
2022-09-26 23:06:56 +0530

    initial commit

---
+++ src/test/java/com/abc/promotion/PromotionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
